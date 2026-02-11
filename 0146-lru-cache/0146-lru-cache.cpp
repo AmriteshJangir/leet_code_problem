a87775c086b3a8b3798de7398e55c9c2ef4c4294
Amritesh Jangir <amriteshjangir@gmail.com>
2026-02-11 14:48:01 +0530

    Time: 105 ms (25.92%), Space: 183.5 MB (22.7%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,95 @@
+// class LRUCache {
+// public:
+//     LRUCache(int capacity) {
+        
+//     }
+    
+//     int get(int key) {
+        
+//     }
+    
+//     void put(int key, int value) {
+        
+//     }
+// };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+class Node {
+public:
+    int key, value;
+    Node* prev;
+    Node* next;
+
+    Node(int k, int v) {
+        key = k;
+        value = v;
+        prev = next = NULL;
+    }
+};
+
+class LRUCache {
+public:
+    int cap;
+    unordered_map<int, Node*> mp;
+    Node* head;
+    Node* tail;
+
+    LRUCache(int capacity) {
+        cap = capacity;
+        head = new Node(-1,-1); // dummy head
+        tail = new Node(-1,-1); // dummy tail
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void addNode(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void deleteNode(Node* node) {
+        Node* p = node->prev;
+        Node* n = node->next;
+        p->next = n;
+        n->prev = p;
+    }
+
+    int get(int key) {
+        if(mp.find(key) == mp.end())
+            return -1;
+
+        Node* node = mp[key];
+        int val = node->value;
+
+        deleteNode(node);
+        addNode(node);
+
+        return val;
+    }
+
+    void put(int key, int value) {
+        if(mp.find(key) != mp.end()) {
+            Node* node = mp[key];
+            deleteNode(node);
+            mp.erase(key);
+        }
+
+        if(mp.size() == cap) {
+            Node* lru = tail->prev;
+            deleteNode(lru);
+            mp.erase(lru->key);
+        }
+
+        Node* newNode = new Node(key,value);
+        addNode(newNode);
+        mp[key] = newNode;
+    }
+};
