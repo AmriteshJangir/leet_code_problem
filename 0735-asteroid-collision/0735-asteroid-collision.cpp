d10fb3b1530433525ce04175a50b3d5cf3d332ef
Amritesh Jangir <amriteshjangir@gmail.com>
2026-02-13 15:20:32 +0530

    Time: 0 ms (100%), Space: 22.3 MB (6.74%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,30 @@
+class Solution{
+    public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+        for (int asteroid : asteroids) {
+            bool destroyed = false;
+            while (!s.empty() && asteroid < 0 && s.top() > 0) {
+                if (s.top() < -asteroid) {
+                    s.pop();
+                } else if (s.top() == -asteroid) {
+                    s.pop();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+            if (!destroyed) {
+                s.push(asteroid);
+            }
+        }
+        vector<int> result(s.size());
+        for (int i = s.size() - 1; i >= 0; --i) {
+            result[i] = s.top();
+            s.pop();
+        }
+        return result;
+    }
+};
