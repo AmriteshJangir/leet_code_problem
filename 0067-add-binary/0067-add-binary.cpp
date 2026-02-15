c82c08a0e744cc3015d713e22d4de7bfcf2286c3
Amritesh Jangir <amriteshjangir@gmail.com>
2026-02-15 20:03:27 +0530

    Time: 4 ms (8.85%), Space: 11 MB (18.57%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res = "";
+        int i = a.size()-1, j = b.size()-1, carry = 0;
+        while(i>=0 || j>=0 || carry){
+            carry += (i>=0 ? a[i--]-'0' : 0);
+            carry += (j>=0 ? b[j--]-'0' : 0);
+            res = char(carry%2 + '0') + res;
+            carry /= 2;
+        }
+        return res;
+    }
+};
