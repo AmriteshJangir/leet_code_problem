2f68b79c2724b5d63b5599bbb753d9027fc9d840
Amritesh Jangir <amriteshjangir@gmail.com>
2026-04-05 19:53:24 +0530

    Time: 0 ms (100%), Space: 10.5 MB (13.45%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        if (moves.length() & 1) return false;
+        unordered_map<char, int> f;
+        for (char c : moves) f[c]++;
+        return f['U'] == f['D'] && f['L'] == f['R'];
+    }
+};
